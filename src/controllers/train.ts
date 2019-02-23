bfab304dacad6ad3aeb6f33f953814b215c4c594
Ruwan Jayasinghe <ruwan800@gmail.com>
2019-02-23 20:59:42 +0000

    train.ts edited online with Bitbucket

--- src/controllers/train.ts
+++ src/controllers/train.ts
@@ -5,7 +5,7 @@ import {TrainPointModel} from "../models/train_point";
 export namespace TrainController {
 
     export async function getTrains(req: Request, res: Response) {
-        let trains = await TrainPointModel.find({}).select("_id line_id position velocity"); // TODO add visible true or user's train_id is equal
+        let trains = await TrainPointModel.find({}).select("_id line_id position velocity timestamp"); // TODO add visible true or user's train_id is equal
         return res.send({success: true, data: trains});
     }
 }
